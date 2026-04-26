efab3570bde56befd47229644508a3aa1fcdf344
shivaaot <shivcarawataot@gmail.com>
2026-04-26 17:48:27 +0530

    fix build errors

--- src/components/ui/PhilosophySection.tsx
+++ src/components/ui/PhilosophySection.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import Button from "../../components/ui/Button";
 
 const PhilosophySection = () => {
   const [expanded, setExpanded] = useState(false);
